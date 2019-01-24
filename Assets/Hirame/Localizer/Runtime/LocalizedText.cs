f9f5db715b35c311bf987f21eb2870960f9d23f7
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2019-01-24 03:22:35 +0200

    Initial commit

---
+++ Assets/Hirame/Localizer/Runtime/LocalizedText.cs
@@ -0,0 +1,35 @@
+﻿using Hirame.Localizer;
+using TMPro;
+using UnityEngine;
+
+public class LocalizedText : MonoBehaviour
+{
+    public string LocalizationKey;
+    public TMP_Text Text;
+    
+    private void OnEnable ()
+    {
+        Localization.LocalizationUpdated += OnLocalizationUpdated;
+        UpdateLocalization ();
+    }
+
+    private void OnDisable ()
+    {
+        Localization.LocalizationUpdated -= OnLocalizationUpdated;
+    }
+
+    private void OnLocalizationUpdated (string localizationKey)
+    {
+        UpdateLocalization ();
+    }
+
+    private void UpdateLocalization ()
+    {
+        Text.text = Localization.GetLocalizedString (LocalizationKey);
+    }
+
+    private void Reset ()
+    {
+        Text = GetComponent<TMP_Text> ();
+    }
+}
---
+++ Assets/Hirame/Localizer/Runtime/Localizer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Hirame.Localizer
+{
+    public static class Localization
+    {
+        public static event System.Action<string> LocalizationUpdated;
+        
+        private static Dictionary<string, string> LocalizedString = new Dictionary<string, string> ();
+        // TODO:
+        // Add support for localized sprites.
+        //private static Dictionary<string, Sprite> LocalizedSprites = new Dictionary<string, Sprite> ();
+
+        public static string GetLocalizedString (string key)
+        {
+            if (LocalizedString.TryGetValue (key, out var value))
+                return value;
+            return $"Missing Localization: {key}.";
+        }
+
+        public static void LoadLocalizationForLanguage (string langKey)
+        {
+            LocalizedString.Clear ();
+
+            var timer = new Stopwatch ();           
+            timer.Start ();
+
+            
+            var path = Path.Combine (Application.streamingAssetsPath, "Localization");
+            Debug.Log (path);
+
+            var dirs = Directory.EnumerateDirectories (path);
+            var langDir = dirs.First (s => s.EndsWith (langKey));
+
+            if (string.IsNullOrEmpty (langDir))
+            {
+                Debug.LogError ($"Lang not found! {langKey}.");
+                return;
+            }
+
+            var files = Directory.GetFiles (langDir, "*.txt");
+
+            foreach (var f in files)
+            {
+                var length = Path.GetFileName (f).Length - 4;
+                var key = Path.GetFileName (f).Substring (0, length);
+
+                var content = File.ReadAllText (f);
+                LocalizedString.Add (key, content);
+            }
+            
+            timer.Stop ();
+            Debug.Log ($"Loaded localization in {timer.ElapsedMilliseconds.ToString ()}ms.");
+
+        }
+        
+        [RuntimeInitializeOnLoadMethod (RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Initialize ()
+        {
+            LoadLocalizationForLanguage ("en_US");                       
+        }
+    }
+    
+}
